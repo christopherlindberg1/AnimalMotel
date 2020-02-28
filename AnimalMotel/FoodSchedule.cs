98b81a586d7196421503aacd0150b28cae8f8d3a
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-02-28 16:14:09 +0100

    Stubbs to FoodSchedule

--- AnimalMotel/FoodSchedule.cs
+++ AnimalMotel/FoodSchedule.cs
@@ -8,6 +8,67 @@ namespace AnimalMotel
 {
     public class FoodSchedule
     {
+        private readonly List<string> _foodDescriptionList = new List<string>();
 
+
+
+        // ======================= Properties ======================= //
+
+
+        public int NrOfFoodDescriptions
+        {
+            get { return _foodDescriptionList.Count; }
+        }
+
+
+
+
+        // ======================= Methods ======================= //
+
+
+        public FoodSchedule()
+        {
+
+        }
+
+        public FoodSchedule(List<string> foodList)
+        {
+
+        }
+
+        public bool AddFoodScheduleItem(string item)
+        {
+            return false;
+        }
+
+        public bool ChangeFoodScheduleItem(string item, int index)
+        {
+            return false;
+        }
+
+        public bool DeleteFoodScheduleItem(string item, int index)
+        {
+            return false;
+        }
+
+        public string DescribeNoFeedingRequired()
+        {
+            return "";
+        }
+
+        public string GetFoodSchedule(int index)
+        {
+            return "";
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateIndex(int index)
+        {
+            return false;
+        }
     }
 }
