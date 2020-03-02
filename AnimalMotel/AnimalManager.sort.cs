71562960572b5f2d00bd8c5012f419ead3063ef0
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-03-02 18:50:05 +0100

    Implemented ascending sorting. Not descending

---
+++ AnimalMotel/AnimalManager.sort.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalMotel
+{
+    public partial class AnimalManager
+    {
+
+        public void SortById()
+        {
+            IOrderedEnumerable<Animal> animals = from animal in _animals
+                                                 orderby animal.Id
+                                                 select animal;
+
+            _animals = animals.ToList<Animal>();
+        }
+
+        public void SortBySpecie()
+        {
+            IOrderedEnumerable<Animal> animals = from animal in _animals
+                                                 orderby animal.GetSpecie()
+                                                 select animal;
+
+            _animals = animals.ToList<Animal>();
+        }
+
+        public void SortByName()
+        {
+            IOrderedEnumerable<Animal> animals = from animal in _animals
+                                                 orderby animal.Name
+                                                 select animal;
+
+            _animals = animals.ToList<Animal>();
+        }
+
+        public void SortByAge()
+        {
+            IOrderedEnumerable<Animal> animals = from animal in _animals
+                                                 orderby animal.Age
+                                                 select animal;
+
+            _animals = animals.ToList<Animal>();
+        }
+
+        public void SortByGender()
+        {
+            IOrderedEnumerable<Animal> animals = from animal in _animals
+                                                 orderby animal.Gender
+                                                 select animal;
+
+            _animals = animals.ToList<Animal>();
+        }
+
+        public void SortBySpecialCharacteristics()
+        {
+            IOrderedEnumerable<Animal> animals = from animal in _animals
+                                                 orderby animal.GetSpecialCharacteristics()
+                                                 select animal;
+
+            _animals = animals.ToList<Animal>();
+        }
+
+
+    }
+}
---
+++ AnimalMotel/Enums/Sorting/SortingParameters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalMotel.Enums.Sorting
+{
+    /// <summary>
+    ///   Enum containing all parameters that animals can
+    ///   get sorted by.
+    /// </summary>
+    public enum SortingParameters
+    {
+        Id,
+        Specie,
+        Name,
+        Age,
+        Gender,
+        Specialcharacteristics,
+    }
+}
