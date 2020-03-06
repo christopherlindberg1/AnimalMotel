a2722510ee3f9cb2ea97bbeac6f76e41bc7c3944
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-03-06 14:23:06 +0100

    Implemented methods from class diagram

--- AnimalMotel/Recipe.cs
+++ AnimalMotel/Recipe.cs
@@ -8,5 +8,42 @@ namespace AnimalMotel
 {
     public class Recipe
     {
+        private readonly ListManager<string> _ingredients = new ListManager<string>();
+        private string _name;
+
+
+
+        // ======================== Properties ======================== //
+        public ListManager<string> Ingredients
+        {
+            get { return _ingredients; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+
+
+        // ======================== Methods ======================== //
+
+        /// <summary>
+        ///   Returns a string representation of a recipe object.
+        /// </summary>
+        /// <returns>String representation of recipe object.</returns>
+        public override string ToString()
+        {
+            StringBuilder recipeString = new StringBuilder();
+
+            for (int i = 0; i < Ingredients.Count; i++)
+            {
+                recipeString.Append($"{ Ingredients[i] }. ");
+            }
+
+            return recipeString.ToString();
+        }
     }
 }
