e5f191b5136c0ab20b14351f154c4c6ef37128af
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-03-14 10:22:12 +0100

    Implementing dictionary

---
+++ AnimalMotel/DictionaryManagerString.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalMotel
+{
+    public class DictionaryManagerString : IDictionaryManagerString
+    {
+        private readonly Dictionary<string, string> _dictionary = new Dictionary<string, string>();
+
+
+
+
+
+        // ========================= Properties ========================= //
+
+        public int Count
+        {
+            get { return Dictionary.Count; }
+        }
+
+        public  Dictionary<string, string> Dictionary
+        {
+            get { return _dictionary; }
+        }
+
+
+
+
+
+        // ========================= Methods ========================= //
+
+        public string this[string key]
+        {
+            get
+            {
+                if (!Dictionary.ContainsKey(key))
+                {
+                    throw new ArgumentException("The provided key does not exist", "key");
+                }
+
+                return Dictionary[key];
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(key))
+                {
+                    throw new ArgumentException("Key cannot be null or empty", "key");
+                }
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Value cannot be null or empty", "value");
+                }
+
+                Dictionary[key] = value;
+            }
+        }
+
+        public string GetItem(string key)
+        {
+            if (!Dictionary.ContainsKey(key))
+            {
+                return null;
+            }
+
+            return Dictionary[key];
+        }
+
+        public bool Add(string key, string value)
+        {
+            if (Dictionary.ContainsKey(key))
+            {
+                return false;
+            }
+
+            Dictionary[key] = value;
+            return true;
+        }
+
+        public bool Change(string key, string value)
+        {
+            if (!Dictionary.ContainsKey(key))
+            {
+                return false;
+            }
+
+            Dictionary[key] = value;
+            return true;
+        }
+
+        public bool Remove(string key)
+        {
+            if (!Dictionary.ContainsKey(key))
+            {
+                return false;
+            }
+
+            Dictionary.Remove(key);
+            return true;
+        }
+    }
+}
